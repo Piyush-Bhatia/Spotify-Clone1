a085328919811f326db208c3de2436108c006157
Piyush Bhatia <70385621+Piyush-Bhatia@users.noreply.github.com>
2024-09-09 19:22:28 +0530

    update

--- fonts.js
+++
@@ -1,5 +0,0 @@
-// Font Family
-font-family: 'Inter', sans-serif;
-font-family: 'Poppins', sans-serif;
-font-family: 'Raleway', sans-serif;
-font-family: 'Roboto', sans-serif;
---
+++ func.js
@@ -0,0 +1,5 @@
+var plusKey = document.querySelector('.plus-button');
+plusKey = 0;
+plusKey.addEventListener('click', ()=>{
+    alert('add button clicked!');
+})
