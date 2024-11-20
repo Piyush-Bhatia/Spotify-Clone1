bc6745f8890ecb73d84e56304d0dd4e10b453d33
Piyush Bhatia <70385621+Piyush-Bhatia@users.noreply.github.com>
2024-11-20 12:41:19 +0530

    update
    testing validation and updating login form

--- login-files/login.js
+++ login-files/login.js
@@ -1,9 +1,12 @@
-const email = document.querySelector("email");
+const loginButton = document.querySelector('.login');
 
-email.addEventListener('click', emailvalidation);
-
-function emailvalidation(output){
-    if(email == ''){
-        prompt("Email cannot be left empyt")
+loginButton.addEventListener('click', function aalert(){
+    const email = document.querySelector('.for-email');
+    const password = document.querySelector('.for-password');
+    const labelEmail = document.querySelector('.label-email');
+    const labelPassword = document.querySelector('.label-password')
+    alert("logged in and saved successfully");
+    if(email == '' || password == ''){
+        
     }
-}
+})
