6b04e73a84d7ed3c2668c8dd28b9649aaa2638f0
Piyush Bhatia <70385621+Piyush-Bhatia@users.noreply.github.com>
2024-11-26 00:10:54 +0530

    update

--- login-files/login.js
+++ login-files/login.js
@@ -4,9 +4,13 @@ loginButton.addEventListener('click', function aalert(){
     const email = document.querySelector('.for-email');
     const password = document.querySelector('.for-password');
     const labelEmail = document.querySelector('.label-email');
-    const labelPassword = document.querySelector('.label-password')
-    alert("logged in and saved successfully");
-    if(email == '' || password == ''){
-        
+    const labelPassword = document.querySelector('.label-password');
+    console.log(`Email: "${email.value.trim()}", Password: "${password.value.trim()}"`);
+    if(email.value.trim() === ' ' || password.value.trim() === ' '){
+        alert('Fields cannot be empty');
+    }else{
+
+        alert(`Email = ${email.value} and password = ${password.value} is valid and successfully logged in`);
     }
-})
+
+    })
