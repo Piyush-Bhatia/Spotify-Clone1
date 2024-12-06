38999c3538c950b5f2833cec78dcf8d8a7982d74
Piyush Bhatia <70385621+Piyush-Bhatia@users.noreply.github.com>
2024-12-06 17:30:17 +0530

    update

--- login-files/login.js
+++ login-files/login.js
@@ -1,28 +1,51 @@
 const loginButton = document.querySelector('.login');
 
-loginButton.addEventListener('click', function aalert(){
+loginButton.addEventListener('click', function(){
+
     const email = document.querySelector('.for-email');
     const password = document.querySelector('.for-password');
-    const emailPattern = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
-    const labelEmail = document.querySelector('.label-email');
-    const labelPassword = document.querySelector('.label-password');
-    console.log(`Email: "${email.value.trim()}", Password: "${password.value.trim()}"`);
-    if(email.value.trim() === '' || password.value.trim() === ''){
-        alert('Fields cannot be empty');
+    const emailPattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    const dummyUser = {
+        emailId : 'piyushbhatia601@gmail.com',
+        password : 'Pbhatia@14'
+    };
+    
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    
+    console.log(`Email is ${emailValue} and Password is ${passwordValue}`);
+    
+    email.style.border = '';
+    password.style.border = '';
+    
+    if(emailValue === '' || passwordValue === ''){
+        alert('Fields cannot be empty.')
+        if(emailValue === '') email.style.border = '2px solid red';
+        if(passwordValue === '') password.style.border = '2px solid red';
+        return;
+    }
+    
+    if(!emailPattern.test(emailValue) && emailValue < 8){
+        alert('please enter a valid emial address.');
+        email.style.border = '2px solid red';
+        return;
+    }
+    if(!passwordPattern.test(passwordValue)){
+        alert('please enter a valid password.');
+        password.style.border = '2px solid red';
+        return;
+    }
+    
+    if(emailValue === dummyUser.emailId && passwordValue === dummyUser.password){
+        alert(`Welcome, ${emailValue}!`);
+        email.style.border = '2px solid green';
+        password.style.border = '2px solid green';
+         
+    }else{
+        alert(`Invalid email or password`);
         email.style.border = '2px solid red';
         password.style.border = '2px solid red';
-    }else{
-        if(email.match(emailPattern) && password.match(passwordPattern)){
-            alert(`Email = ${email.value} and password = ${password.value} is valid and successfully logged in`);
-        }
-        else {
-            if(email.match(!emailPattern)){
-                alert('incorrect email');
-            }
-            else{
-                alert("incorrect password");
-            }
-        }
     }
-
 })
+
